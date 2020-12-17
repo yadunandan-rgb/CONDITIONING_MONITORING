3e9340877344898badc0110ad02caf6cf9c72247
nidhik38 <61276448+nidhik38@users.noreply.github.com>
2020-12-17 18:12:40 +0530

    Add files via upload

---
+++ bearing_analysis/MQTT_bearing_with_class_check_version2.py
@@ -0,0 +1,208 @@
+import paho.mqtt.client as mqtt  #import the client1
+import time
+from configparser import ConfigParser
+import logging
+import sys
+from collections import deque
+import logging.handlers as handlers
+import pandas as pd
+import datetime
+import json
+import numpy as np
+import newclassmqtt as fun
+
+###input parameters  is taken from configration parser##########
+config_object = ConfigParser() 
+config_object.read(r"D:/bearingtestingnew/userinfo2edited.ini")
+
+####calling the userinfo object.
+userinfo= config_object["user_info2"] 
+
+###calling the server info object. 
+serverinfo= config_object["server_config"] 
+
+####calling the sensor info object.
+sensor_info=config_object["sensor_info"] 
+Bearing_info=config_object["bearing_info"] 
+
+###accessing the broker address from the config file. 
+Broker_address=serverinfo["broker_address"] 
+
+###accessing the broker port from the config file. 
+Broker_port=int(serverinfo["broker_port"]) 
+
+###accessing the sensorid from the config file. 
+Sensor_id=sensor_info["sensor_id"] 
+
+###accessing the sampling frequency from the config file. 
+samplingFrequency=int(sensor_info["samplingfrequency"]) 
+
+### for subscribing the fft data accessing the tagname from the config file. 
+Tag_name=sensor_info["tag_name"] 
+
+### for publishing the alerts/faultlevel through mqtt accessing the publish tagname from the config file. 
+pubishing_tag=sensor_info['publish_tag_name'] 
+
+####accessing the mode of the sensor from the config file.
+Mode =sensor_info["mode"] 
+
+####accessing the number of samples of the sensor from the config file.
+nsamp =int(sensor_info["no_samples"])
+
+####accessing the window size of the sensor from the config file.
+windowsize =int(sensor_info['window_size'])
+
+#Number of Rolling Element or Ball of bearing
+
+NB=int(Bearing_info["NB"]) 
+
+#Rolling Element or Ball Diameter of bearing
+BD=float(Bearing_info["BD"]) 
+
+#pitch circle diameter of the bearing 
+PD=float(Bearing_info["PD"]) 
+
+#Contact Angle
+angle=int(Bearing_info["angle"]) 
+shaftspeed=int(Bearing_info["shaftspeed"])
+power=int(userinfo["p"]) #power of motor
+
+#############derived variables from the config file #######################
+###number of windows per burst
+nwindows=int(nsamp/windowsize)
+timetocompleteoneburst=int(nsamp)/int(samplingFrequency)
+output=[]
+##time difference between one burst to another
+time_diff_btw_windows=timetocompleteoneburst/nwindows
+
+#####################
+###it is used to append the accelerometer data
+queue = deque()
+
+
+###it is used to append epoch time of data
+queue1 = deque()
+     
+#####function for handling log functions ####################
+def on_log(client, userdata, level,buf):
+    logging.basicConfig(filename='MQTT_bearinginfo.log', level=logging.DEBUG,format='%(asctime)s - %(levelname)s - %(message)s',datefmt='%m/%d/%Y %I:%M:%S %p')
+    logger = logging.getLogger()
+    log_formatter = logging.Formatter('%(asctime)s%(levelname)s %(message)s')
+    logHandler = handlers.RotatingFileHandler(str(Sensor_id)+'.log', maxBytes=1500, backupCount=5)
+    logHandler.setFormatter(log_formatter)
+    logHandler.setLevel(logging.INFO)
+    logger.handlers.clear()
+    logger.addHandler(logHandler)
+    logger.setLevel(logging.INFO)
+    logger.propagate=False
+
+
+######function for receiving the disconnection messages ######### 
+def on_disconnect(client, userdata, rc):
+    logging.info("broker disconnected Returned code = "  +str(rc))
+    client.connected_flag=False
+    client.disconnect_flag=True  
+
+######this fiunction connects the mqtt broker #####
+def on_connect(client, userdata, flags, rc):
+    if rc==0:
+        client.connected_flag=True #set flag
+        # logging.info("MQTTconnection - connected OK")
+    else:
+        logging.info("Bad connection Returned code=",rc)#pgm of bearing fault
+
+#######function for receiving the data from the broker, once message is recieved it is stored in queque
+def on_message(client, userdata, msg):
+    queue.append((json.loads(msg.payload.decode().split("$", 2)[0]))['f'])
+    queue1.append(msg.payload.decode().split("$", 2)[2])
+
+
+mqtt.Client.connected_flag=False 
+mqtt.Client.bad_connection_flag=False #
+client = mqtt.Client()  
+client.on_connect = on_connect ####calling mqtt connection function
+client.on_message = on_message ####calling data receiving function
+client.on_disconnect=on_disconnect #####calling disconnection function
+client.on_log=on_log #########calling log function for handling logs 
+
+client.loop_start()
+try:
+    client.connect(Broker_address, Broker_port) #connect to broker
+    while not client.connected_flag and not client.bad_connection_flag: #wait in loop
+        time.sleep(4)
+
+#####when there is no internet,the below message will log in to the logger. 
+except: 
+    logging.info("MQTTconnection -not established :please check")
+    # print('cant connect')
+    sys.exit("quit")
+
+###for continuous loop running defined runflag as true
+run_flag=True
+# count=1
+# array=[]
+while run_flag:
+    ###subscribing the topic #####
+    client.subscribe(Tag_name, qos=1)
+    
+   
+
+    sys_tme=datetime.datetime.now()
+    ###epoch time of accelerometer data
+    df=pd.DataFrame(list(queue1),columns=['time_epoch'])
+    ###dropping duplicate values  
+    df=df.drop_duplicates(keep='first',inplace=False)
+    ###converting datatype of epochtime  to float
+    df['time_epoch']=df['time_epoch'].astype(float)
+    ###converting epoch time to datetime
+    df['time'] =df['time_epoch'].map(lambda val: datetime.datetime.fromtimestamp(val).strftime('%Y-%m-%d %H:%M:%S.%f'))
+    df['time']=pd.to_datetime(df['time'])
+    FFT_list = set(tuple(x) for x in list(queue))
+    FFT_list = [ list(x) for x in FFT_list]
+    if len(FFT_list) >= nwindows:
+        ###averaging the fft data
+        
+        bearing=fun.Bearing_analysis(NB,BD,PD,angle,shaftspeed,samplingFrequency,nsamp,windowsize,FFT_list)
+        
+        f1,b1,i1,o1=bearing.bearing_total(FFT_list)
+        #client.publish(pubishing_tag,o1)
+        if sum([len(f1),len(b1),len(i1),len(o1)]) == 0: #checks whether list are empty 
+            print("bearing is healthy")
+        else:
+            print("bearing has some problem")
+
+        f1.clear()
+        b1.clear() #clears all the list 
+        i1.clear()
+        o1.clear()
+                
+
+    else:
+        if len(FFT_list)>0:
+            time_diff=(sys_tme-df['time'].iloc[-1]).total_seconds()#current sys-latest window
+            # print('time dif',time_diff)
+            
+            if time_diff >60:
+                # print('length is ',len(FFT_list),FFT_list)
+                ###averaging the fft data
+                average= [sum(e)/len(e) for e in zip(*FFT_list)]  
+                print(average)  
+                bearing=fun.Bearing_analysis(NB,BD,PD,angle,shaftspeed,samplingFrequency,nsamp,windowsize,FFT_list)
+
+                f1,b1,i1,o1=bearing.bearing_total(FFT_list)
+                #client.publish(pubishing_tag,o1)
+                if sum([len(f1),len(b1),len(o1),len(i1)]) == 0: #checks whether list are empty 
+                    print("bearing is healthy")
+                else:
+                    print("bearing has some problem")
+
+                f1.clear()
+                b1.clear() #clears all the list 
+                i1.clear()
+                o1.clear()
+                        
+    time.sleep(time_diff_btw_windows)
+client.loop_stop()
+####disconnecting the broker
+client.disconnect()
+
---
+++ bearing_analysis/Mqtt_class_bearing_version2.py
@@ -0,0 +1,231 @@
+import paho.mqtt.client as mqtt  #import the client1
+import time
+from configparser import ConfigParser
+import logging
+import sys
+from collections import deque
+import logging.handlers as handlers
+import pandas as pd
+import datetime
+import json
+import numpy as np
+import math
+"""nb=4
+bd=0.76
+p_d=0.56
+angles=0
+Shaftspeed=83
+samplingFrequency=1600
+nsamp=4096
+windowsize=512
+brokerport=1883
+brokeraddress="176.19.34.01"
+tagname="A434F17EE90B/FFTX"
+nwindows=nsamp/windowsize
+lim_value=0.3
+fftlist=[2.3,3.4,5.3,3.4,3.04]
+timetocompleteoneburst=nsamp/samplingFrequency
+time_diff_btw_windows=timetocompleteoneburst/nwindows"""
+
+class Bearing_analysis():
+
+    def __init__(self,NB,BD,PD,angle,shaftspeed,samplingFrequency,nsamp,windowsize,fftlist):
+        self.Limit_value=float(0.3)
+        self.N_B=int(NB)
+        self.B_D=float(BD)
+        self.P_D=float(PD)
+        self.Angle=float(angle)
+        self.Shaft_Speed=shaftspeed
+        self.Sampling_Frequency=samplingFrequency
+        self.No_sample=nsamp
+        self.Window_Size = windowsize
+        self.FFT_list=fftlist
+        self.resultant=[]
+        self.resultant1=[]
+        self.resultant2=[]
+        self.resultant3=[]
+        
+#####function for handling log functions ####################
+    
+
+    def ftf_range(self): #NB,BD,PD,ANGLE are bearing inputs
+        try:
+            self.Fcir=((1/2)*(1-(self.B_D/self.P_D)*math.cos(self.Angle)))
+            self.FTF=self.Fcir*self.Shaft_Speed #fundamental train frequency
+            self.FTFFinal=float(self.FTF)
+            rangelimit=self.FTFFinal*self.Limit_value
+            self.FTFFinalrange1=float(self.FTFFinal+rangelimit)
+            self.FTFFinalrange2=float(self.FTFFinal-rangelimit)
+            return self.FTFFinalrange1,self.FTFFinalrange2
+        except Exception as e:
+            print(str(e))    
+        #print(FTFFinal)
+    def bsf_range(self): #NB,BD,PD,ANGLE are bearing inputs
+        try:
+            self.Bsf=((self.P_D/(2*self.B_D))*(1-((self.B_D/self.P_D)*(math.cos(self.Angle)))**2))
+            self.BSF=self.Bsf*self.Shaft_Speed #ball spin frequency
+            self.BSFFinal=float(self.BSF)
+            rangelimit=self.BSFFinal*self.Limit_value
+            self.BSFFinalrange1=float(self.BSFFinal+rangelimit)
+            self.BSFFinalrange2=float(self.BSFFinal-rangelimit)
+            return self.BSFFinalrange1,self.BSFFinalrange2
+        except Exception as e:
+            print(str(e))
+    def bpfi_range(self):   
+        #print(BSFFinal) 
+        try:
+            self.Bfir=((self.N_B/2)*(1+(self.B_D/self.P_D)*math.cos(self.Angle)))
+            self.BPFI=self.Bfir*self.Shaft_Speed #ball pass frequency of inner race
+            self.BPFIFinal=float(self.BPFI)
+            rangelimit=self.BPFIFinal*self.Limit_value
+            self.BPFIFinalrange1=float(self.BPFIFinal+rangelimit)
+            self.BPFIFinalrange2=float(self.BPFIFinal-rangelimit)
+            return self.BPFIFinalrange1,self.BPFIFinalrange2
+        except Exception as e:
+            print(str(e))
+        #(FTFFinal)
+        
+        #print(BPFIFinal) 
+    def bpfo_range(self):      
+        try:
+
+            self.Bfor=((self.N_B/2)*(1-(self.B_D/self.P_D)*math.cos(self.Angle)))
+            self.BPFO=self.Bfor*self.Shaft_Speed #ball pass frequency  of outer race
+            self.BPFOFinal=float(self.BPFO)
+            rangelimit=self.BPFOFinal*self.Limit_value
+            self.BPFOFinalrange1=float(self.BPFOFinal+rangelimit)
+            self.BPFOFinalrange2=float(self.BPFOFinal-rangelimit)
+            return self.BPFOFinalrange1,self.BPFOFinalrange2
+        except Exception as e:
+            print(str(e))    
+        #print(BPFOFinal) 
+        
+        
+
+        #return BPFIFinal
+####on passing averaged fft values ,getting corresponding frequencies of amplitudes which are above the RMS value.
+##fftavg=averaged fft values ####samplingFrequency of the sensor ###window size of the data.
+
+    
+    def FFT_calculations(self,fftdata):
+        try:
+            self.average= [sum(e)/len(e) for e in zip(*fftdata)]
+            self.xf = np.linspace(0.0, 1.0 / (2.0 * (1/self.Sampling_Frequency)), len(self.average))  #frequencies selection
+            ####fftavg should be in pandas.core.series.Series
+            self.rms =np.sqrt(np.mean(np.square(self.average))) #fft values are averaged
+            self.Amplitude_rms_index = [list(self.average).index(i) for i in self.average if i>3*self.rms] #checks for amplitudes index greater thn 3*rms
+            #print(Amplitude_rms_index)
+            self.HorizontAmplitudAbove_rms_values = [i for i in self.average if i>3*self.rms] #checks for the amplitudes for the above index values
+            #print(HorizontAmplitudAbove_rms_values)
+            self.corres_frequency = [list(self.xf)[i] for i in self.Amplitude_rms_index] #corresponding freqns of amplitudes greater than 3*rms
+            #print(corres_frequency)
+            return self.corres_frequency #returns the ampltide,frqns, and rms 
+        except Exception as e:
+            print(str(e))
+    def FTF_analysis(self,range1,range2,frequency): #checks whether ftf therotical frequencies  range matches the the frqns greater then rms obtained from the parser
+        #self.corres_frequency=frequency
+        try:
+            global result
+            for x in frequency: 
+                
+                if x>=range1 and x<=range2: #first harmonic check
+                    result= "first harmonic FTF fault"
+                    
+                    self.resultant.append(result)
+                if x>= 2*range1 and x<= 2*range2:##second harmonic check
+                    result="2nd harmonic ftf fault"
+                    #print("2nd ftf")
+                    self.resultant.append(result)
+
+                if x>= 3*range1 and x<= 3*range2: ##third harmonic check
+                    result="3rd harmonic FTF fault"
+                    #print("3rd ftf")
+                    self.resultant.append(result)
+
+            return self.resultant   
+        except Exception as e:
+            print(str(e))                
+    def BSF_analysis(self,range1,range2,frequency): #checks whether bsf therotical frequencies  range matches the the frqns greater then rms obtained from the parser
+        #self.corres_frequency=frequency
+        try:
+            global result
+            
+            for x in frequency:
+                if x>=range1 and x<=range2: #first harmonic check
+                    result= "first harmonic bsf fault"
+                    #print("first bsf")
+                    self.resultant1.append(result)
+
+                if x>= 2*range1 and x<= 2*range2:#second harmonic check
+                    result="2nd harmonic bsf fault"
+                    
+                    #print("2nd bsf")
+                    self.resultant1.append(result)
+
+                    #return (result)
+                if x>= 3*range1 and x<= 3*range2: #third harmonic check
+                    result="3rd harmonic bsf fault"
+                    #print("3rd bsf")
+                    self.resultant1.append(result)
+            return self.resultant1   
+        except Exception as e:
+            print(str(e))
+    def OUTERRACE_analysis(self,range1,range2,frequency):#checks whether outerrace therotical frequencies  range matches the the frqns greater then rms obtained from the parser
+        #self.corres_frequency=frequency
+        try:
+            global result
+            for x in frequency:
+                if x>= range1 and x<=range2: #first harmonic check
+                    result= "first harmonic outerrace fault"
+                    #print("first outerrace")
+                    self.resultant2.append(result)
+
+                if x>= 2*range1 and x<= 2*range2: #second harmonic check
+                    result="2nd harmonic outerrace fault"
+                    #print("2nd outerrace")
+                    self.resultant2.append(result)
+
+                if x>= 3*range1 and x<= 3*range2:#third harmonic check
+                    result="3rd harmonic outerrace fault"
+                    #print("3rd outerrace")
+                    self.resultant2.append(result)
+            return self.resultant2   
+        except Exception as e:
+            print(str(e))
+                            
+    def INNERRACE_analysis(self,range1,range2,frequency): #checks whether innerrace therotical frequencies  range matches the the frqns greater then rms obtained from the parser
+        #self.corres_frequency=frequency
+        try:
+            global result
+            for x in frequency:
+                if x>= range1 and x<=range2:   #first harmonic check
+                    result= "first harmonic innerrace fault"
+                    #print("first innerrace")
+                    self.resultant3.append(result)
+
+                if x>= 2*range1 and x<= 2*range2: #second harmonic check
+                    result="2nd harmonic innerrace fault"
+                    #print("2nd innerrace")
+                    self.resultant3.append(result)
+
+                if x>= 3*range1 and x<= 3*range2: #third harmonic check
+                    result="3rd harmonic innerrace fault" 
+                    #print("3rd innerrace")
+                    self.resultant3.append(result)
+            return self.resultant3
+        except Exception as e:
+            print(str(e)) 
+    def bearing_total(self,fftdata):
+        try:
+            ftfrange1,ftfrange2=self.ftf_range()
+            bsfrange1,bsfrange2=self.bsf_range()
+            bpfirange1,bpfirange2=self.bpfi_range()
+            bpforange1,bpforange2=self.bpfo_range()
+            fft_frqn=self.FFT_calculations(fftdata)
+            bear_ftfresult=self.FTF_analysis(ftfrange2,ftfrange1,fft_frqn)
+            bear_bsfresult=self.BSF_analysis(bsfrange2,bsfrange1,fft_frqn)
+            bear_bpfiresult=self.INNERRACE_analysis(bpfirange2,bpfirange1,fft_frqn)
+            bear_bpforesult=self.OUTERRACE_analysis(bpforange2,bpforange1,fft_frqn)
+            return bear_ftfresult,bear_bsfresult,bear_bpfiresult,bear_bpforesult
+        except Exception as e:
+            print(str(e))
